31184496a90138714716f3a173d5132d783de133
ArielPerezGalvez <arielgalvez70@yahoo.es>
2018-05-11 13:29:07 -0600

    Empezando el proyecto

---
+++ Lab#3_ArielPerez/src/lab/pkg3_arielperez/Lab3_ArielPerez.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg3_arielperez;
+import java.util.Scanner;
+import java.util.ArrayList;
+/**
+ *
+ * @author ariel
+ */
+public class Lab3_ArielPerez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+        
+        
+    }//fin del main
+    
+}//fin de la clase
